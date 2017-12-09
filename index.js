0dc13a1b20c9a2445024bf3f4538d1a32d44b7da
Jeff Bernstein <jeffreybbernstein@gmail.com>
2017-12-09 11:54:41 -0500

    got basic promise system working for kanji api calls

--- index.js
+++ index.js
@@ -9,13 +9,12 @@ function startApp() {
 function watchSubmit() {
   $('.js-search-form').submit(event => {
     event.preventDefault();
-    hideStuff();
-    clearDivs();
+    $('.js-results').html('');
     let englishWord = $(event.currentTarget)
       .find('.js-query')
       .val();
-    $('.js-word-english').html(englishWord);
-    getWordFromApi(englishWord, processWordData);
+    getWordFromApi(englishWord, processDataFromWordApi);
+    // highlight word instead of clearing it on search?
     $(event.currentTarget)
       .find('.js-query')
       .val('');
@@ -34,46 +33,153 @@ function getWordFromApi(searchTerm, callback) {
   $.ajax(query);
 }
 
-function processWordData(data) {
-  console.log(data);
-  let wordData = [];
-  let size = data.data.length >= 5 ? 5 : data.data.length;
-  for (let i = 0; i < size; i++) {
-    wordData[i] = data.data[i];
-  }
-  console.log(wordData);
+function processDataFromWordApi(data) {
+  $('.results').fadeIn(FADE_TIME);
+  processWordData(data.data);
 }
 
-function requestKanjiData(charArray, charLabelArray, containsKanji) {
-  if (containsKanji) {
-    let kanjiArray = [];
-    charArray.forEach((char, index) => {
-      if (charLabelArray[index] === 'kanji') kanjiArray.push(char);
+function processWordData(wordArray) {
+  let currentWordArray = wordArray;
+  let wordsToDisplay = [];
+  if (wordArray.length <= 5) {
+    wordsToDisplay = currentWordArray;
+    let kanjiArray = wordsToDisplay.map((word, index) => {
+      return identifyKanji(word);
     });
-    kanjiArray.forEach(char =>
-      getKanjiInfoFromApi(char, displayKanjiSearchData)
+    let kanjiArrayWithLocation = addKanjiLocation(kanjiArray);
+    console.log(kanjiArrayWithLocation);
+    let kanjiPromiseArray = kanjiArrayWithLocation.map(kanjiObj =>
+      getKanjiInfoFromApi(kanjiObj.kanji)
     );
+    Promise.all(kanjiPromiseArray).then((...args) => {
+      console.log(args);
+      for (let i = 0; i < args.length; i++) {
+        const kanjiData = args[i];
+        const location = kanjiArrayWithLocation[i];
+      }
+    });
+    // pull in kanji api data with indexes
+    let kanjiInfo = 'data from apis';
+    // pass them to display function so it can display info
+    displayWordData(wordsToDisplay, kanjiInfo);
+  } else {
+    wordsToDisplay = currentWordArray.splice(0, 5);
+
+    let kanjiInfo = 'data from apis';
+    displayWordData(wordsToDisplay, kanjiInfo);
+    // execute listener to load more results on scroll down
+    // pageScrollListener(currentWordArray)
   }
 }
 
-function getKanjiInfoFromApi(searchTerm, callback) {
-  const query = {
-    headers: {
-      'X-Mashape-Key': 'KCKQ5WNODBmshLeydUQgzK645yIOp1a4IPpjsnOsnNPVb3ini0'
-    },
-    url: 'https://kanjialive-api.p.mashape.com/api/public/kanji/' + searchTerm,
-    dataType: 'json',
-    complete: callback
-  };
-  $.ajax(query);
+function identifyKanji(word) {
+  if (typeof word.japanese[0].word === 'undefined') return [''];
+  let wordCharacters = word.japanese[0].word.split('');
+  let kanjiInWord = wordCharacters.filter((char, index) => {
+    return (
+      (char >= '\u4e00' && char <= '\u9faf') ||
+      (char >= '\u3400' && char <= '\u4dbf')
+    );
+  });
+  return kanjiInWord;
 }
 
-function displayKanjiSearchData(data) {
+function addKanjiLocation(kanjiArray) {
+  let kanjiArrayWithLocation = [];
+  let kanjiCounter = 0;
+  kanjiArray.forEach((kanjiInWord, wordIndex) => {
+    kanjiInWord.forEach((kanji, kanjiIndex) => {
+      kanjiArrayWithLocation[kanjiCounter] = {
+        kanji: kanji,
+        location: [wordIndex, kanjiIndex]
+      };
+      kanjiCounter++;
+    });
+  });
+  return kanjiArrayWithLocation;
+}
+
+function displayWordData(wordArray, kanjiInfo) {
+  wordArray.forEach((word, index) => {
+    let japaneseWord = word.japanese[0].word;
+    let wordReading = word.japanese[0].reading;
+    let wordRomaji = wanakana.toRomaji(wordReading);
+    let wordDefinitions = word.senses.map((definition, index) => {
+      return definition.english_definitions.join(', ');
+    });
+
+    let wordResultSection = '';
+    if (typeof japaneseWord === 'undefined') {
+      wordResultSection = `
+        <span class="japanese-word-hiragana">${wordReading}</span> (word)<br>
+        <span class="japanese-word-romaji">${wordRomaji}</span> (romaji)<br>`;
+    } else {
+      wordResultSection = `
+        <span class="japanese-word">${japaneseWord}</span> (word)<br>
+        <span class="japanese-word-hiragana">${wordReading}</span> (reading)<br>
+        <span class="japanese-word-romaji">${wordRomaji}</span> (romaji)<br>`;
+    }
+    let definitionSection = wordDefinitions
+      .map((definition, index) => {
+        return `<div class="definition">${index + 1}. ${definition}</div>`;
+      })
+      .join('');
+    let kanjiSection = '';
+    let $resultDiv = $(`
+      <div class="row">
+        <div class="col-3">
+          <div class="word-result">
+            ${wordResultSection}
+          </div>
+        </div>
+        <div class="col-5">
+          <div class="definition-result">
+            ${definitionSection}
+          </div>
+        </div>
+        <div class="col-4">
+          ${kanjiSection}
+        </div>
+      </div>
+      <hr>
+    `);
+    $resultDiv
+      .hide()
+      .appendTo('.js-results')
+      .fadeIn(FADE_TIME);
+  });
+}
+
+function pageScrollListener(wordArray) {
+  // set this infinite scroll listener up
+  $('place to look').submit(event => {
+    processWordData(wordArray);
+  });
+}
+
+function getKanjiInfoFromApi(searchTerm) {
+  return new Promise((resolve, reject) => {
+    const query = {
+      headers: {
+        'X-Mashape-Key': 'KCKQ5WNODBmshLeydUQgzK645yIOp1a4IPpjsnOsnNPVb3ini0'
+      },
+      url:
+        'https://kanjialive-api.p.mashape.com/api/public/kanji/' + searchTerm,
+      dataType: 'json',
+      complete: function(data) {
+        resolve(data);
+      },
+      error: function(data) {
+        resolve();
+      }
+    };
+    $.ajax(query);
+  });
+}
+
+function processKanjiData(data) {
   let kanjiData = JSON.parse(data.responseText);
-  if (typeof kanjiData.kanji === 'undefined') {
-    $('.js-kanji').text(`Sorry, the character isn't in our database!`);
-    return;
-  }
+  if (typeof kanjiData.kanji === 'undefined') return '';
   let kanjiChar = kanjiData.kanji.character;
   let kanjiVid = kanjiData.kanji.video.mp4;
   let kanjiVidPoster = kanjiData.kanji.video.poster;
@@ -81,49 +187,23 @@ function displayKanjiSearchData(data) {
   let kanjiStrokes = kanjiData.kanji.strokes.count;
   let kanjiGrade = kanjiData.references.grade;
   if (kanjiGrade === null) kanjiGrade = 'not listed';
-  let $kanjiDiv = $(`
-    <div class='row'>
-      <div class='col-md-6'>
-        <video width="320" height="240" controls poster="${kanjiVidPoster}">
-          <source src="${kanjiVid}" type="video/mp4">
-          Your browser does not support the video tag.</video>
+  let kanjiGroup = `
+      <div class="kanji-group">
+        <div class="kanji-col-left">
+          <div class="kanji-result">
+            <video width="320" height="240" controls poster="${kanjiVidPoster}">
+              <source src="${kanjiVid}" type="video/mp4">
+              Your browser does not support the video tag.</video>
+          </div>
+        </div>
+        <div class="kanji-col-right">
+          meaning: ${kanjiMeaning}
+          <br>strokes: ${kanjiStrokes}
+          <br>grade: ${kanjiGrade}
+        </div>
       </div>
-      <div class='col-md-6'>
-        <div>Meaning: ${kanjiMeaning}</div>
-        <div>Strokes: ${kanjiStrokes}</div>
-        <div>Grade Level: ${kanjiGrade}</div>
-      </div>
-    </div>`);
-  $kanjiDiv
-    .hide()
-    .appendTo('.js-kanji')
-    .fadeIn(FADE_TIME);
-}
-
-function fadeInContent() {
-  $('.word').fadeIn(FADE_TIME);
-  $('.learn-more').fadeIn(FADE_TIME);
-  if (!$('.js-romaji').is(':empty')) $('.romaji').fadeIn(FADE_TIME);
-  if (!$('.js-kanji').is(':empty')) $('.kanji').fadeIn(FADE_TIME);
-  if (!$('.js-hiragana').is(':empty')) $('.hiragana').fadeIn(FADE_TIME);
-  if (!$('.js-katakana').is(':empty')) $('.katakana').fadeIn(FADE_TIME);
-}
-
-function hideStuff() {
-  $('.word').hide();
-  $('.romaji').hide();
-  $('.learn-more').hide();
-  $('.kanji').hide();
-  $('.hiragana').hide();
-  $('.katakana').hide();
-}
-
-function clearDivs() {
-  $('.js-word').html('');
-  $('.js-romaji').html('');
-  $('.js-kanji').html('');
-  $('.js-hiragana').html('');
-  $('.js-katakana').html('');
+    `;
+  return { index, kanjiGroup };
 }
 
 $(startApp);
@@ -239,4 +319,23 @@ $(startApp);
 //   let wordRomaji = wanakana.toRomaji(wordFurigana);
 //   $('.js-romaji').text(wordRomaji);
 //   fadeInContent();
+// }
+// function requestKanjiData(charArray, charLabelArray, containsKanji) {
+//   if (containsKanji) {
+//     let kanjiArray = [];
+//     charArray.forEach((char, index) => {
+//       if (charLabelArray[index] === 'kanji') kanjiArray.push(char);
+//     });
+//     kanjiArray.forEach(char =>
+//       getKanjiInfoFromApi(char, displayKanjiSearchData)
+//     );
+//   }
+// }
+// function fadeInContent() {
+//   $('.word').fadeIn(FADE_TIME);
+//   $('.learn-more').fadeIn(FADE_TIME);
+//   if (!$('.js-romaji').is(':empty')) $('.romaji').fadeIn(FADE_TIME);
+//   if (!$('.js-kanji').is(':empty')) $('.kanji').fadeIn(FADE_TIME);
+//   if (!$('.js-hiragana').is(':empty')) $('.hiragana').fadeIn(FADE_TIME);
+//   if (!$('.js-katakana').is(':empty')) $('.katakana').fadeIn(FADE_TIME);
 // }
